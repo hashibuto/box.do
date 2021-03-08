b0186962bbaa39bb80b4258ee7df5d4ac1aabbf7
Philip Stefou <hashibuto@noreply.com>
2021-03-09 08:39:53 +0900

    adding digitalocean api base code

--- config/config.go
+++ config/config.go
@@ -24,6 +24,9 @@ type Config struct {
 	VolumeSize         int
 	DropletSlug        string
 	BareDomainName     string
+	PrivateKeyFilename string
+	ImageID            string
+	PublicKeyID        string
 }
 
 const defaultRegion = region.NYC3
@@ -44,12 +47,15 @@ func getConfigFilename(projectName string) string {
 	return fmt.Sprintf("%v.yml", projectName)
 }
 
+// getTextInput prompts the user for a single line of input, then validates said input.
+// if validation fails, then the user is prompted again (this loop continues until input is valid).
 func getTextInput(prompt string, validationFunc func(string) bool) (string, error) {
 	result := false
 	var input string
 	var err error
 	for result == false {
-		fmt.Print(prompt)
+		fmt.Println(prompt)
+		fmt.Print(">")
 		reader := bufio.NewReader(os.Stdin)
 		input, err = reader.ReadString('\n')
 		if err != nil {
@@ -64,11 +70,12 @@ func getTextInput(prompt string, validationFunc func(string) bool) (string, erro
 	return input, nil
 }
 
+// NewConfig returns a new config struct based on user input
 func NewConfig(projectName string) (*Config, error) {
 	config := Config{}
 
 	apiKey, err := getTextInput(
-		"DigitalOcean API key\n>",
+		"DigitalOcean API key",
 		func(value string) bool {
 			return len(value) > 0
 		},
@@ -82,7 +89,7 @@ func NewConfig(projectName string) (*Config, error) {
 		fmt.Printf("%v	%v\n", r, region.GetName(r))
 	}
 	r, err := getTextInput(
-		fmt.Sprintf("Deployment region (enter for default %v)\n>", defaultRegion),
+		fmt.Sprintf("Deployment region (enter for default %v)", defaultRegion),
 		func(value string) bool {
 			return (len(value) == 0 ||
 				region.IsValid(value))
@@ -99,7 +106,7 @@ func NewConfig(projectName string) (*Config, error) {
 	}
 
 	volumeStr, err := getTextInput(
-		"Volume size in gigabytes (minimum 1)\n>",
+		"Volume size in gigabytes (minimum 1)",
 		func(value string) bool {
 			i, err := strconv.Atoi(value)
 			if err != nil {
@@ -118,7 +125,7 @@ func NewConfig(projectName string) (*Config, error) {
 		fmt.Println(d)
 	}
 	d, err := getTextInput(
-		fmt.Sprintf("Droplet slug (enter for default %v)\n>", defaultDroplet),
+		fmt.Sprintf("Droplet slug (enter for default %v)", defaultDroplet),
 		func(value string) bool {
 			return (len(value) == 0 ||
 				droplet.IsValid(value))
@@ -133,7 +140,7 @@ func NewConfig(projectName string) (*Config, error) {
 	config.DropletSlug = d
 
 	bareDomain, err := getTextInput(
-		"Bare domain name (eg: mysite.com)\n>",
+		"Bare domain name (eg: mysite.com)",
 		func(value string) bool {
 			// Simple regular expression to reject trivial cases, not meant to be thorough
 			matched, err := regexp.Match("^\\w+\\.\\w{1,3}$", []byte(value))
@@ -147,6 +154,29 @@ func NewConfig(projectName string) (*Config, error) {
 		return nil, err
 	}
 	config.BareDomainName = bareDomain
+
+	homeDir, _ := os.UserHomeDir()
+	defaultKeyFile := path.Join(homeDir, ".ssh", "id_rsa")
+	pkFilename, err := getTextInput(
+		fmt.Sprintf("Private key full path (Enter for default %v)", defaultKeyFile),
+		func(value string) bool {
+			filename := value
+			if len(value) == 0 {
+				filename = defaultKeyFile
+			}
+			_, err := os.Stat(filename)
+			if err != nil {
+				fmt.Println("Unable to locate or access keyfile")
+				return false
+			}
+
+			return true
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	config.PrivateKeyFilename = pkFilename
 
 	configDir, err := GetConfigDir()
 	if err != nil {
@@ -162,6 +192,29 @@ func NewConfig(projectName string) (*Config, error) {
 	err = ioutil.WriteFile(configFilePath, configBytes, os.FileMode(0600))
 	if err != nil {
 		return nil, fmt.Errorf("Unable to write configuration file", configFilePath)
+	}
+
+	return &config, nil
+}
+
+// LoadConfig loads the YAML configuration associated with the project name, or returns an error
+func LoadConfig(projectName string) (*Config, error) {
+	configDir, err := GetConfigDir()
+	if err != nil {
+		return nil, err
+	}
+
+	configFilePath := path.Join(configDir, getConfigFilename(projectName))
+
+	configData, err := ioutil.ReadFile(configFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read file at %v, are you sure the project exists?", configFilePath)
+	}
+
+	config := Config{}
+	err = yaml.Unmarshal(configData, &config)
+	if err != nil {
+		return nil, err
 	}
 
 	return &config, nil
